db439ac3544fba30fc478fdd7f6a0c52ee25ec17
gttrcr <gttrcr@gmail.com>
2024-01-16 19:47:41 +0100

    git submodule foreach git pull origin master

--- Operations.cs
+++ Operations.cs
@@ -30,6 +30,7 @@ namespace GitSync
                     MutexConsole.WriteLine("Pull (" + currentBranch + ")...", line);
                     Run(null, "git -C " + repoPath + " pull");
                     Run(null, "git -C " + repoPath + " submodule update --recursive --remote --init");
+                    Run(null, "git -C " + repoPath + " submodule foreach git pull origin master");
                     MutexConsole.WriteLine("Check...", line);
                     if (Run(null, "git -C " + repoPath + " diff --stat").Count > 0)
                     {
