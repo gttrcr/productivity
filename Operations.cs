d495f7d2179e1b2f73c7433dc2abda14550fb08d
gttrcr <gttrcr@gmail.com>
2024-01-16 15:14:53 +0100

    Improve with not found on remote repos

--- Operations.cs
+++ Operations.cs
@@ -1,4 +1,4 @@
-using static GttrcrGist.MutexConsole;
+using GttrcrGist;
 using static GttrcrGist.Process;
 
 namespace GitSync
@@ -15,43 +15,43 @@ namespace GitSync
 
             try
             {
-                MutexConsoleWriteLine(repoPath + "...", line, ConsoleColor.White, zeroLeft);
+                MutexConsole.WriteLine(repoPath + "...", line, ConsoleColor.White, zeroLeft);
                 if (!Directory.Exists(repoPath))
                 {
-                    MutexConsoleWriteLine("Clone...", line);
+                    MutexConsole.WriteLine("Clone...", line);
                     Run(null, "git clone --recurse-submodules -j8 git@github.com:" + organization + "/" + name + ".git " + repoPath);
-                    MutexConsoleWriteLine("done", line, ConsoleColor.Green);
+                    MutexConsole.WriteLine("done", line, ConsoleColor.Green);
                 }
                 else
                 {
                     List<string> localBranches = Run(null, "git -C " + repoPath + " branch --format='%(refname:short)'");
                     List<string> remoteBranches = Run(null, "git -C " + repoPath + " branch -r --format='%(refname:short)'");
                     string currentBranch = Run(null, "git -C " + repoPath + " rev-parse --abbrev-ref HEAD")[0];
-                    MutexConsoleWriteLine("Pull (" + currentBranch + ")...", line);
+                    MutexConsole.WriteLine("Pull (" + currentBranch + ")...", line);
                     Run(null, "git -C " + repoPath + " pull");
                     Run(null, "git -C " + repoPath + " submodule update --recursive --remote --init");
-                    MutexConsoleWriteLine("Check...", line);
+                    MutexConsole.WriteLine("Check...", line);
                     if (Run(null, "git -C " + repoPath + " diff --stat").Count > 0)
                     {
                         SomeDiff ??= new List<Repo>();
                         SomeDiff.Add(new() { Organization = organization, Name = name, Path = repoPath });
-                        MutexConsoleWriteLine("some diff...", line, ConsoleColor.Yellow);
+                        MutexConsole.WriteLine("some diff...", line, ConsoleColor.Yellow);
                         if (push)
                         {
-                            MutexConsoleWriteLine("push...", line, ConsoleColor.Yellow);
+                            MutexConsole.WriteLine("push...", line, ConsoleColor.Yellow);
                             Run(null, "git -C " + repoPath + " add .");
                             Run(null, "git -C " + repoPath + " commit -m 'gitsync auto push'");
                             Run(null, "git -C " + repoPath + " push");
                         }
                     }
-                    MutexConsoleWriteLine("done", line, ConsoleColor.Green);
+                    MutexConsole.WriteLine("done", line, ConsoleColor.Green);
                 }
             }
             catch
             {
                 if (iterations == 0)
                 {
-                    MutexConsoleWriteLine("unable to complete", line, ConsoleColor.Red);
+                    MutexConsole.WriteLine("unable to complete", line, ConsoleColor.Red);
                     UnableToComplete ??= new List<Repo>();
                     UnableToComplete.Add(new() { Organization = organization, Name = name, Path = repoPath });
                 }
--- Program.cs
+++ Program.cs
@@ -2,7 +2,8 @@
 using System.Text.Json;
 using static GttrcrGist.Process;
 using static GitSync.Operations;
-using static GttrcrGist.MutexConsole;
+using GttrcrGist;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GitSync
 {
@@ -23,7 +24,7 @@ namespace GitSync
                 throw new Exception("Cannot find configuration file " + args[0]);
 
             if (args.Length > 1)
-                MutexConsoleWriteLine("Skipped parameters after the first");
+                MutexConsole.WriteLine("Skipped parameters after the first");
 
             configFile = args[0];
         }
@@ -33,7 +34,7 @@ namespace GitSync
             try
             {
                 InitialChecks(args, out string configFile);
-                ResetScreen();
+                MutexConsole.Clear();
                 Config? orgs = JsonSerializer.Deserialize<Config>(File.ReadAllText(configFile));
                 if (orgs == null)
                     throw new Exception("null organization file");
@@ -42,48 +43,61 @@ namespace GitSync
                 {
                     string organization = orgs.Value.Repos.ElementAt(i).Organization;
                     List<string> repos = orgs.Value.Repos.ElementAt(i).Repos;
+                    List<string> remoteReposList = new(Run(new() {
+                        new() { OSPlatform = OSPlatform.Linux, Command = "gh repo list " + organization + " | awk '{ print $1 }'"  }
+                    }));
+
+                    if (remoteReposList.Count == 0)
+                    {
+                        MutexConsole.WriteLine("No remote repos for organization " + organization, null, ConsoleColor.Yellow);
+                        continue;
+                    }
+
+                    remoteReposList = remoteReposList.Select(x => x.Split('/').Last()).ToList();
+
                     if (repos.Count == 1 && repos[0].Equals("*"))
-                        orgs.Value.Repos[i] = new()
-                        {
-                            Organization = organization,
-                            Repos = new List<string>(Run(new() { new()
-                            {
-                                OSPlatform = OSPlatform.Linux, Command = "gh repo list " + organization + " | awk '{ print $1 }'"  // command for linux
-                            } }))
-                        };
+                        orgs.Value.Repos[i] = new() { Organization = organization, Repos = new(remoteReposList) };
+                    else
+                    {
+                        List<string> mismatchRepos = repos.Except(remoteReposList).ToList();
+                        mismatchRepos.ForEach(x => MutexConsole.WriteLine("Remote repo " + x + " in organization " + organization + " does not exists", null, ConsoleColor.Yellow));
+                        orgs.Value.Repos[i] = new() { Organization = organization, Repos = new(repos.Except(mismatchRepos)) };
+                    }
                 }
 
                 int line = 0;
                 Parallel.ForEach(orgs.Value.Repos, new ParallelOptions { MaxDegreeOfParallelism = 2 }, x => Parallel.ForEach(x.Repos, new ParallelOptions { MaxDegreeOfParallelism = 3 }, y => UpdateRepo(x.Organization, y, orgs.Value.Path, line++, false, 5, args.Length == 2 && args[1].Equals("push"))));
-                MutexConsoleWriteLine("Done" + Environment.NewLine, line);
-                ResetScreen();
+                MutexConsole.WriteLine("Done", line);
+                MutexConsole.Clear();
 
-                //Last messages
+                //Attention repositories
                 if (SomeDiff == null || SomeDiff.Count == 0)
-                    MutexConsoleWriteLine("Everything is up to date", null, ConsoleColor.Green);
+                    MutexConsole.WriteLine("Everything is up to date", null, ConsoleColor.Green);
                 else
-                    MutexConsoleWriteLine("The following repos need attention", null, ConsoleColor.Yellow);
-                SomeDiff?.ForEach(x => MutexConsoleWriteLine(x.Organization + "\t" + x.Name + "\t" + x.Path));
+                    MutexConsole.WriteLine("The following repos need attention", null, ConsoleColor.Yellow);
+                SomeDiff?.OrderBy(x => x.Organization).ToList().ForEach(x => MutexConsole.WriteLine(x.Organization + "\t" + x.Name + "\t" + x.Path));
 
+                //Error repositories
                 if (UnableToComplete != null && UnableToComplete.Count != 0)
-                    MutexConsoleWriteLine("The following repos are in error", null, ConsoleColor.Red);
-                UnableToComplete?.ForEach(x => MutexConsoleWriteLine(x.Organization + "\t" + x.Name + "\t" + x.Path));
+                    MutexConsole.WriteLine("The following repos are in error", null, ConsoleColor.Red);
+                UnableToComplete?.OrderBy(x => x.Organization).ToList().ForEach(x => MutexConsole.WriteLine(x.Organization + "\t" + x.Name + "\t" + x.Path));
 
+                //Not found repositories on remote
                 orgs.Value.Repos.ForEach(x =>
                 {
                     List<string> dirs = Directory.GetDirectories(Path.Combine(orgs.Value.Path, x.Organization)).ToList();
                     List<string> expectedFolders = x.Repos.Select(y => Path.Combine(orgs.Value.Path, x.Organization, y)).ToList();
-                    List<string> diffFolders = dirs.Except(expectedFolders).ToList();
-                    if (diffFolders.Count > 0)
+                    List<string> mismatchFolders = dirs.Except(expectedFolders).ToList();
+                    if (mismatchFolders.Count > 0)
                     {
-                        MutexConsoleWriteLine("Furthermore, these folders are not repositories of " + x.Organization, null, ConsoleColor.Yellow);
-                        diffFolders?.ForEach(x => MutexConsoleWriteLine(x));
+                        MutexConsole.WriteLine("Furthermore, these folders are not remote repositories of " + x.Organization, null, ConsoleColor.Yellow);
+                        mismatchFolders?.ForEach(x => MutexConsole.WriteLine(x));
                     }
                 });
             }
             catch (Exception ex)
             {
-                MutexConsoleWriteLine(ex.Message);
+                MutexConsole.WriteLine(ex.Message, null, ConsoleColor.Red);
             }
         }
     }
