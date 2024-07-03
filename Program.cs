a29371695c39e8e7a4592916b8e9670a63be0415
gttrcr <gttrcr@gmail.com>
2024-07-03 09:44:33 +0200

    CheckForInternetConnection

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using System.Text.Json;
 using static GttrcrGist.Process;
 using static GitSync.Operations;
 using GttrcrGist;
+using System.Globalization;
+using System.Net;
 
 namespace GitSync
 {
@@ -32,6 +34,35 @@ namespace GitSync
             }
 
             configFile = args[0];
+
+            if (!CheckForInternetConnection())
+                throw new Exception("Not internet connection");
+        }
+
+        public static bool CheckForInternetConnection(int timeoutMs = 10000, string? url = null)
+        {
+            try
+            {
+                url ??= CultureInfo.InstalledUICulture switch
+                {
+                    { Name: var n } when n.StartsWith("fa") => // Iran
+                        "http://www.aparat.com",
+                    { Name: var n } when n.StartsWith("zh") => // China
+                        "http://www.baidu.com",
+                    _ =>
+                        "http://www.gstatic.com/generate_204",
+                };
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.KeepAlive = false;
+                request.Timeout = timeoutMs;
+                using var response = (HttpWebResponse)request.GetResponse();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public static void Main(string[] args)
